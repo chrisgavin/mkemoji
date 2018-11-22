e53d7a82feac150007d5d794fad424f648cdd95a
Chris Gavin <chris@chrisgavin.me>
2018-11-22 13:04:47 +0000

    Calculate scaling manually to avoid breaking animated emoji.

--- mkemoji/transformations.py
+++ mkemoji/transformations.py
@@ -3,7 +3,13 @@ from wand.image import Image
 def transform(image:Image, max_resolution:int) -> Image:
 	image.trim()
 
-	resize_geometry = "%dx%d>" % (max_resolution, max_resolution)
-	image.transform(resize=resize_geometry)
+	width = image.width
+	height = image.height
+	scale_factor = max_resolution / max(width, height)
+	scale_factor = min(1, scale_factor)
+
+	width = int(width * scale_factor)
+	height = int(height * scale_factor)
+	image.resize(width, height)
 
 	return image
